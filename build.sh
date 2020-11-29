c2cc5c97eaf8c0768ee078c8551f0946b2d5db3a
sezanzeb <proxima@hip70890b.de>
2020-11-29 12:23:07 +0100

    debian package

--- build.sh
+++ build.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
 
-# build the .deb and .appimage files
-# https://ubuntuforums.org/showthread.php?t=1002909
-dist=dist
-name=key-mapper-0.1.0
+build_deb() {
+  # https://github.com/phusion/debian-packaging-for-the-modern-developer/tree/master/tutorial-1
+  dist=dist
+  name=key-mapper-0.1.0
 
-python3 setup.py sdist --dist-dir $dist
-tar -C deb -xzf $dist/$name.tar.gz
-cp $dist/DEBIAN $dist/$name -r
-dpkg-deb -b $dist/$name $name.deb
-rm $dist/$name -r
+  python3 setup.py sdist --dist-dir $dist
+  tar -C $dist -xzf $dist/$name.tar.gz
+  rm $dist/$name.tar.gz
+  cp deb/DEBIAN $dist/$name -r
+  dpkg-deb -b $dist/$name $dist/$name.deb
+  rm $dist/$name -r
+  cp $dist/$name.deb deb
+  echo ".deb package created"
+}
 
+build_appimage() {
+
+}
+
+
+build_deb &
+build_appimage &
+
+wait
