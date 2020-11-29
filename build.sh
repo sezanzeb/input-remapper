9afc70ed6f9faeac48af1c4eac2a61f79370127d
sezanzeb <proxima@hip70890b.de>
2020-11-29 12:16:08 +0100

    wip debian package

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 # build the .deb and .appimage files
 # https://ubuntuforums.org/showthread.php?t=1002909
-dist=deb
+dist=dist
 name=key-mapper-0.1.0
 
-python3 setup.py sdist --dist-dir deb
+python3 setup.py sdist --dist-dir $dist
 tar -C deb -xzf $dist/$name.tar.gz
 cp $dist/DEBIAN $dist/$name -r
 dpkg-deb -b $dist/$name $name.deb
