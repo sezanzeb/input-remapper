2231a5320b92300dfcb4a8c601c046751be81851
sezanzeb <proxima@hip70890b.de>
2020-11-29 10:44:51 +0100

    wip debian package

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # build the .deb and .appimage files
 # https://ubuntuforums.org/showthread.php?t=1002909
 
-python setup.py sdist
+python3 setup.py sdist
 cd dist
 tar -xzf key-mapper-0.1.0.tar.gz
 cd key-mapper-0.1.0
