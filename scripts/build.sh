74e1d83dc66c22f9023477c76d6cfe3de1e8b9ba
sezanzeb <proxima@hip70890b.de>
2021-02-07 00:07:08 +0100

    update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,12 +3,13 @@
 build_deb() {
   # https://www.devdungeon.com/content/debian-package-tutorial-dpkgdeb
   # that was really easy actually
-  rm build dist -r
+  rm build -r
   mkdir build/deb -p
   python3 setup.py install --root=build/deb
   mv build/deb/usr/local/lib/python3.*/ build/deb/usr/lib/python3/
   cp ./DEBIAN build/deb/ -r
   mkdir dist -p
+  rm dist/key-mapper-0.6.1.deb || true
   dpkg -b build/deb dist/key-mapper-0.6.1.deb
 }
 
