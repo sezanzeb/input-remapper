feaa6dda261245f3474a5874d8310c28efddef57
Jonas Bosse <jonas.bosse@posteo.de>
2022-01-22 17:40:16 +0100

    indented asyncio.sleep in consumer_control

--- inputremapper/injection/consumer_control.py
+++ inputremapper/injection/consumer_control.py
@@ -92,17 +92,17 @@ class ConsumerControl:
                 # a listener might be blocking forever while waiting for more events.
                 asyncio.ensure_future(listener(event))
                 
-            # Running macros have priority, give them a head-start for processing the
-            # event.  If if_single injects a modifier, this modifier should be active
-            # before the next handler injects an "a" or something, so that it is
-            # possible to capitalize it via if_single.
-            # 1. Event from keyboard arrives (e.g. an "a")
-            # 2. the listener for if_single is called
-            # 3. if_single decides runs then (e.g. injects shift_L)
-            # 4. The original event is forwarded (or whatever it is supposed to do)
-            # 5. Capitalized "A" is injected.
-            # So make sure to call the listeners before notifying the handlers.
-            await asyncio.sleep(0)
+                # Running macros have priority, give them a head-start for processing the
+                # event.  If if_single injects a modifier, this modifier should be active
+                # before the next handler injects an "a" or something, so that it is
+                # possible to capitalize it via if_single.
+                # 1. Event from keyboard arrives (e.g. an "a")
+                # 2. the listener for if_single is called
+                # 3. if_single decides runs then (e.g. injects shift_L)
+                # 4. The original event is forwarded (or whatever it is supposed to do)
+                # 5. Capitalized "A" is injected.
+                # So make sure to call the listeners before notifying the handlers.
+                await asyncio.sleep(0)
 
             tasks = []
             results = []
