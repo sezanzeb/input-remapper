162e7fcbf7e1ae951fc43e89521e92c39351b07a
sezanzeb <proxima@sezanzeb.de>
2022-02-06 23:52:27 +0100

    Avoiding GObject dependencies for unit tests

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -1,1 +1,7 @@
 """Tests that require a linux desktop environment to be running."""
+
+import gi
+
+gi.require_version("Gtk", "3.0")
+gi.require_version("GLib", "2.0")
+gi.require_version("GtkSource", "4")
