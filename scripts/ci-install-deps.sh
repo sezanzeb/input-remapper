d1030d8ffe49791a3494ead1992d75c3a6c64c98
Luna Nova <git@lunnova.dev>
2022-02-06 17:36:27 -0800

    Install subset of python deps with apt-get for ci

--- scripts/ci-install-deps.sh
+++ scripts/ci-install-deps.sh
@@ -3,16 +3,12 @@
 set -xeuo pipefail
 
 # native deps
-# gettext required to generate translations
-sudo apt-get install -y gettext
+# gettext required to generate translations, others are python deps
+sudo apt-get install -y gettext python3-evdev python3-pydbus python3-pydantic
 
 # ensure pip and setuptools/wheel up to date so can install all pip modules
 python -m pip install --upgrade pip
 pip install wheel setuptools
-
-# install input-remapper's deps from setup.py
-python setup.py egg_info
-pip install `grep -v '^\[' *.egg-info/requires.txt`
 
 # install test deps which aren't in setup.py
 pip install psutil
