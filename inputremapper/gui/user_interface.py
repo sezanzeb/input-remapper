8e36ec7a665620a172a16214456170f9d68943fb
Te Wang <kearkyle@gmail.com>
2022-03-31 00:19:36 -0700

    Check macro syntax now working when user type new input into text field

--- inputremapper/gui/user_interface.py
+++ inputremapper/gui/user_interface.py
@@ -206,7 +206,7 @@ class UserInterface:
         source_view = self.get("code_editor")
 
         # throw debounce on typing
-        source_view.get_buffer().connect("changed", self.check_macro_syntax)
+        source_view.get_buffer().connect("changed", self.check_on_typing)
 
         # if any of the next steps take a bit to complete, have the window
         # already visible (without content) to make it look more responsive.
@@ -473,12 +473,14 @@ class UserInterface:
             status_bar.set_tooltip_text(tooltip)
 
     @debounce
+    def check_on_typing(self, *_):
+        self.check_macro_syntax()
+        self.editor.gather_changes_and_save()
+
     def check_macro_syntax(self):
         """Check if the programmed macros are allright."""
         self.show_status(CTX_MAPPING, None)
-        print("check_macro_syntax")
         for key, output in active_preset:
-            print("checking", key, output)
             output = output[0]
             if not is_this_a_macro(output):
                 continue
