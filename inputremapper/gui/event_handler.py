267ce83efd17a128024582e13d16a1524728c2e9
Jonas Bosse <jonas.bosse@posteo.de>
2022-04-17 16:11:00 +0200

    Implement GuiEventHandler

--- inputremapper/gui/event_handler.py
+++ inputremapper/gui/event_handler.py
@@ -1,5 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# input-remapper - GUI for device specific keyboard mappings
+# Copyright (C) 2022 sezanzeb <proxima@sezanzeb.de>
+#
+# This file is part of input-remapper.
+#
+# input-remapper is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# input-remapper is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with input-remapper.  If not, see <https://www.gnu.org/licenses/>.
 import enum
-from typing import Callable, Awaitable
+from typing import Callable, Dict, Set
+
+
+from inputremapper.logger import logger
 
 
 class GuiEvents(str, enum.Enum):
@@ -9,19 +31,28 @@ class GuiEvents(str, enum.Enum):
     test_ev2 = "test_event2"
 
 
-GuiEventListener = Callable[[...], Awaitable]
+GuiEventListener = Callable[[...], None]
 
 
 class GuiEventHandler:
+    def __init__(self):
+        self._listeners: Dict[GuiEvents, Set[GuiEventListener]] = {
+            event: set() for event in GuiEvents
+        }
 
     def emit(self, event: GuiEvents, **kwargs) -> None:
-        pass
-
-    def emit_blocking(self, event: GuiEvents, **kwargs) -> None:
-        pass
+        for listener in self._listeners[event]:
+            listener(**kwargs)
 
     def subscribe(self, event: GuiEvents, listener: GuiEventListener) -> None:
-        pass
+        if event not in self._listeners:
+            raise KeyError(event)
+        logger.debug("adding new GuiEventListener: %s", listener)
+        self._listeners[event].add(listener)
 
     def unsubscribe(self, listener: GuiEventListener) -> None:
-        pass
+        for listeners in self._listeners.values():
+            try:
+                listeners.remove(listener)
+            except KeyError:
+                pass
