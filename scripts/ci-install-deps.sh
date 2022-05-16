ad96eba6909cea34d8d5c90902d781f25b2fbc5a
Jonas Bosse <jonas.bosse@posteo.de>
2022-05-17 00:44:33 +0200

    and more ...

--- scripts/ci-install-deps.sh
+++ scripts/ci-install-deps.sh
@@ -4,7 +4,7 @@ set -xeuo pipefail
 
 # native deps
 # gettext required to generate translations, others are python deps
-sudo apt-get install -y gettext python3-evdev python3-pydbus python3-pydantic python3-gi gir1.2-gtk-3.0
+sudo apt-get install -y gettext python3-evdev python3-pydbus python3-pydantic python3-gi gir1.2-gtk-3.0 gir1.2-gtksource-4
 
 # ensure pip and setuptools/wheel up to date so can install all pip modules
 python -m pip install --upgrade pip
