63d4720e6adb56ead1ec3836b4da39f70b740975
sezanzeb <proxima@sezanzeb.de>
2023-02-11 16:20:51 +0100

    fix chunked string for translation

--- inputremapper/gui/controller.py
+++ inputremapper/gui/controller.py
@@ -260,9 +260,11 @@ class Controller:
             self.show_status(
                 CTX_WARNING,
                 _("ctrl, alt and shift may not combine properly"),
-                _("Your system might reinterpret combinations ")
-                + _("with those after they are injected, and by doing so ")
-                + _("break them."),
+                _(
+                    "Your system might reinterpret combinations " +
+                    "with those after they are injected, and by doing so " +
+                    "break them."
+                ),
             )
 
     def move_input_config_in_combination(
