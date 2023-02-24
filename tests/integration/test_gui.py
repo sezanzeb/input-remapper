fdb16d82ed5d4b3f4f9826d58eee95f3f7881f67
sezanzeb <proxima@sezanzeb.de>
2023-02-24 18:05:32 +0100

    black

--- tests/integration/test_gui.py
+++ tests/integration/test_gui.py
@@ -2028,10 +2028,10 @@ class TestAutocompletion(GuiTestBase):
         self.user_interface.autocompletion.navigate(None, event)
 
     def get_suggestions(self, autocompletion):
-        return ([
-            row.get_children()[0].get_text() for row in
-            autocompletion.list_box.get_children()
-        ])
+        return [
+            row.get_children()[0].get_text()
+            for row in autocompletion.list_box.get_children()
+        ]
 
     def test_get_incomplete_parameter(self):
         def test(text, expected):
@@ -2041,11 +2041,11 @@ class TestAutocompletion(GuiTestBase):
             text_iter.set_offset(len(text))
             self.assertEqual(get_incomplete_parameter(text_iter), expected)
 
-        test('bar(foo', 'foo')
-        test('bar(a=foo', 'foo')
-        test('bar(qux, foo', 'foo')
-        test('foo', 'foo')
-        test('bar + foo', 'foo')
+        test("bar(foo", "foo")
+        test("bar(a=foo", "foo")
+        test("bar(qux, foo", "foo")
+        test("foo", "foo")
+        test("bar + foo", "foo")
 
     def test_autocomplete_names(self):
         autocompletion = self.user_interface.autocompletion
@@ -2057,13 +2057,13 @@ class TestAutocompletion(GuiTestBase):
             text_iter = self.code_editor.get_iter_at_location(0, 0)[1]
             text_iter.set_offset(len(text))
 
-        setup('disa')
-        self.assertNotIn('KEY_A', self.get_suggestions(autocompletion))
-        self.assertIn('disable', self.get_suggestions(autocompletion))
+        setup("disa")
+        self.assertNotIn("KEY_A", self.get_suggestions(autocompletion))
+        self.assertIn("disable", self.get_suggestions(autocompletion))
 
-        setup(' + _A')
-        self.assertIn('KEY_A', self.get_suggestions(autocompletion))
-        self.assertNotIn('disable', self.get_suggestions(autocompletion))
+        setup(" + _A")
+        self.assertIn("KEY_A", self.get_suggestions(autocompletion))
+        self.assertNotIn("disable", self.get_suggestions(autocompletion))
 
     def test_autocomplete_key(self):
         self.controller.update_mapping(output_symbol="")
