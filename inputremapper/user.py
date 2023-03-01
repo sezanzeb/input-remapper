78015a419253f9fcc9c5d3868cfb57049c822b9c
sezanzeb <proxima@sezanzeb.de>
2023-03-01 20:48:07 +0100

    upgrade copyright year

--- inputremapper/user.py
+++ inputremapper/user.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # input-remapper - GUI for device specific keyboard mappings
-# Copyright (C) 2022 sezanzeb <proxima@sezanzeb.de>
+# Copyright (C) 2023 sezanzeb <proxima@sezanzeb.de>
 #
 # This file is part of input-remapper.
 #
