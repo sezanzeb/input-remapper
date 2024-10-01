203edb2078bfa9421d6276f23a48ede5e5aad676
sezanzeb <28510156+sezanzeb@users.noreply.github.com>
2024-10-02 00:36:45 +0200

    kasjdfa

--- tests/unit/test_daemon.py
+++ tests/unit/test_daemon.py
@@ -17,6 +17,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with input-remapper.  If not, see <https://www.gnu.org/licenses/>.
+
 from evdev._ecodes import EV_ABS
 
 from inputremapper.input_event import InputEvent
