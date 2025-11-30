d03eff29a1781b73515e831c6fb95183e06a53ce
sezanzeb <28510156+sezanzeb@users.noreply.github.com>
2025-11-30 12:12:00 +0100

    black

--- inputremapper/installation_info.py
+++ inputremapper/installation_info.py
@@ -18,9 +18,9 @@
 # You should have received a copy of the GNU General Public License
 # along with input-remapper.  If not, see <https://www.gnu.org/licenses/>.
 
-COMMIT_HASH = 'unknown'
-VERSION = '2.2.0'
+COMMIT_HASH = "unknown"
+VERSION = "2.2.0"
 # depending on where this file is installed to, make sure to use the proper
 # prefix path for data. Package maintainers may overwrite this file in
 # any way they like.
-DATA_DIR = '/usr/share/inputremapper'
+DATA_DIR = "/usr/share/inputremapper"
