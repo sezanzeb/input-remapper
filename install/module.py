d7f2c07406a29191b26333ea9c236ee58a521832
sezanzeb <28510156+sezanzeb@users.noreply.github.com>
2025-12-10 15:22:59 +0100

    Improved sys.path ranking

--- install/module.py
+++ install/module.py
@@ -46,33 +46,37 @@ from install.data_files import DATA_DIR
 
 
 def _key(path) -> int:
+    # bad
+
+    if not os.path.isdir(path):
+        # /usr/lib/python313.zip or a directory that doesn't exist
+        return 5
+
+    if not path.startswith("/"):
+        # Editable package paths, not system-wide, user installations which don't work
+        # for input-remapper.
+        return 4
+
+    if path.startswith("/home") or path.startswith("/root"):
+        return 3
+
+    if path.startswith("/usr/local"):
+        # Cannot be imported in udev and some python installations.
+        # Workarounds are annoying and not satisfactory.
+        return 2
+
+    if not '-packages' in path:
+        # Don't install into the standard libraries path (such as /usr/lib/python3.13)
+        return 1
+
     # good
 
-    if path.startswith("/usr/lib/python3/") or path == "/usr/lib/python3":
+    if path.startswith("/usr/lib/python3/"):
         # Paths that work independent of the python version, yes please
         return -2
 
     if path.startswith("/usr/lib"):
         return -1
-
-    # bad
-
-    if path.startswith("/usr/local"):
-        # Cannot be imported in udev and some python installations.
-        # Workarounds are annoying and not satisfactory.
-        return 1
-
-    if path.startswith("/home") or path.startswith("/root"):
-        return 2
-
-    if not path.startswith("/"):
-        # Editable package paths, not system-wide, user installations which don't work
-        # for input-remapper.
-        return 3
-
-    if not os.path.isdir(path):
-        # /usr/lib/python313.zip
-        return 4
 
     # neutral
     # Stuff like /usr/lib/python3.13/site-packages
