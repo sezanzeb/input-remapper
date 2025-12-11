726d29e930f7535703615be1e99bdf68b33fbc6b
sezanzeb <28510156+sezanzeb@users.noreply.github.com>
2025-12-11 22:31:56 +0100

    Try ci without setuptools

--- scripts/ci-install-deps.sh
+++ scripts/ci-install-deps.sh
@@ -7,7 +7,7 @@ set -xeuo pipefail
 sudo apt-get install -y gettext python3-evdev python3-dasbus python3-pydantic python3-gi gir1.2-gtk-3.0 gir1.2-gtksource-4
 
 # ensure pip and setuptools/wheel up to date so can install all pip modules
-sudo apt-get install python3-pip python3-wheel python3-setuptools
+sudo apt-get install python3-pip python3-wheel
 
 # install test deps which aren't in pyproject.toml
 python -m pip install psutil pylint-pydantic
