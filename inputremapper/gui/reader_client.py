e30e5dc912ef2ee58ff3abca6de151c6634a4103
sezanzeb <28510156+sezanzeb@users.noreply.github.com>
2026-08-04 12:25:56 +0200

    copyright notice update

--- inputremapper/gui/reader_client.py
+++ inputremapper/gui/reader_client.py
@@ -1,5 +1,5 @@
 # input-remapper - GUI for device specific keyboard mappings
-# Copyright (C) 2025 sezanzeb <b8x45ygc9@mozmail.com>
+# Copyright (C) 2026 sezanzeb <4t1pzast9@mozmail.com>
 #
 # This file is part of input-remapper.
 #
