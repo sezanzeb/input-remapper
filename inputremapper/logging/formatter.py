f6cd14d168f1c05d82abfff24a6d4240fee94998
sezanzeb <28510156+sezanzeb@users.noreply.github.com>
2026-09-11 17:20:36 +0200

    logging error

--- inputremapper/logging/formatter.py
+++ inputremapper/logging/formatter.py
@@ -121,7 +121,7 @@ class ColorfulFormatter(logging.Formatter):
 
         process_color = self._word_to_color(f"{os.getpid()}{sys.argv[0]}")
 
-        now = datetime.datetime.now(tz=datetime.timezone.utc)
+        now = datetime.now(tz=datetime.timezone.utc)
         return (
             f'{now.strftime("%H:%M:%S.%f")} '
             f"\033[38;5;{process_color}m"  # color
